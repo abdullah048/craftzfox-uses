7efbf4b069b3a79456c5aca75051415bbec2aa62
IB1148981 <103644657+IB1148981@users.noreply.github.com>
2022-09-24 11:34:22 +0500

    feat(blogpost.astro): styled BlogPost.astro and added rehype-classes for tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,29 @@ import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://craftzfox-uses.vercel.app',
-  integrations: [sitemap(), react(), image(), tailwind()]
+  integrations: [sitemap(), react(), image(), tailwind()],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-mplus',
+          h2: 'text-2xl font-bold font-mplus',
+          h3: 'text-xl font-bold font-mplus',
+          h4: 'text-lg font-bold font-mplus',
+          h5: 'font-bold font-mplus',
+          h6: 'font-bold font-mplus',
+          img: 'border border-slate-300 dark:border-zinc-500 rounded-lg mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-purple-500 decoration-purple-500 hover:dark:text-teal-500 dark:decoration-teal-500'
+        }
+      ]
+    ]
+  }
 })
